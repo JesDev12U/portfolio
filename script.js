8ea7e3a252f3502d9c6a2d2a920d8d159172ce9f
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-03-25 03:18:44 -0600

    Inicio

---
+++ script.js
@@ -0,0 +1,15 @@
+const typed = new Typed(".typed", {
+  strings: ["Jesus Antonio Lopez Bandala", ""],
+  loop: true,
+  typeSpeed: 75,
+  backSpeed: 75,
+  backdelay: 2000,
+});
+
+const typed2 = new Typed(".typed2", {
+  strings: ["Informatic", "Developer", "FullStack"],
+  loop: true,
+  typeSpeed: 75,
+  backSpeed: 75,
+  backdelay: 2000,
+});
