a0d14fac7d14b6f47f13de62fe95685b950ab2c7
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-03-26 12:12:47 -0600

    Menu hamburguesa nav responsive

---
+++ responsive.js
@@ -0,0 +1,11 @@
+const $nav = document.getElementById("nav");
+const $open = document.getElementById("open");
+const $close = document.getElementById("close");
+
+$open.addEventListener("click", () => {
+  $nav.classList.add("visible");
+});
+
+$close.addEventListener("click", () => {
+  $nav.classList.remove("visible");
+});
