3b15bfda773fc539ebcc8e1da23e35395dffaedf
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-03-25 23:30:28 -0600

    Terminal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".typed", {
-  strings: ["Jesus Antonio Lopez Bandala", ""],
+  strings: ["Jesus Antonio Lopez Bandala", "JesDev12U"],
   loop: true,
   typeSpeed: 75,
   backSpeed: 75,
@@ -8,6 +8,18 @@ const typed = new Typed(".typed", {
 
 const typed2 = new Typed(".typed2", {
   strings: ["Informatic", "Developer", "Fullstack"],
+  loop: true,
+  typeSpeed: 75,
+  backSpeed: 75,
+  backdelay: 2000,
+});
+
+const typed3 = new Typed(".typed3", {
+  strings: [
+    "Welcome to my portfolio!",
+    "In this site you can see my projects",
+    "I hope you like it!",
+  ],
   loop: true,
   typeSpeed: 75,
   backSpeed: 75,
