3a2a83b2eaa7549500c1b8b6e99d3387c1d0fcaf
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2025-05-15 19:58:37 -0600

    Actualización al Portafolio

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const typed = new Typed(".typed", {
 });
 
 const typed2 = new Typed(".typed2", {
-  strings: ["Informatic", "Developer", "Fullstack"],
+  strings: ["Informático", "Desarrollador", "Fullstack"],
   loop: true,
   typeSpeed: 75,
   backSpeed: 75,
@@ -16,9 +16,9 @@ const typed2 = new Typed(".typed2", {
 
 const typed3 = new Typed(".typed3", {
   strings: [
-    "Welcome to my portfolio!",
-    "In this site you can see my projects",
-    "I hope you like it!",
+    "Bienvenido a mi portafolio!",
+    "En este sitio podrás ver mis proyectos y más",
+    "¡Espero les guste!",
   ],
   loop: true,
   typeSpeed: 75,
@@ -54,7 +54,3 @@ nextButton.addEventListener("click", () => {
 });
 
 updateProject();
-
-setInterval(() => {
-  nextButton.click();
-}, 5000);
